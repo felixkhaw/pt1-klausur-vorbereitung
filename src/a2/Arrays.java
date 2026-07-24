8cb41422d74c20823284e23da0207fbfe9abe020
felixkhaw <felix.knupper@haw-hamburg.de>
2026-07-24 15:46:15 +0200

    Add Tests and eval for last tasks

--- src/a2/Arrays.java
+++ src/a2/Arrays.java
@@ -1,15 +1,7 @@
 package a2;
 
 public class Arrays {
-    public static void main() {
-        int[][] m = {
-                {7,1,-3,0,-6},
-                {4,-5,9,-1,2},
-                {-3,8,3,6,-4}
-        };
-        int [][] a = ausschnitt(m,0,2,2,5);
-        Arrays.toString(a);
-    }
+    
     public static int[][] ausschnitt(int[][] m, int zv, int zb, int sv, int sb){
         int anz_z = zb - zv;
         int anz_s = sb - sv;
---
+++ src/a3/Datum.java
@@ -0,0 +1,7 @@
+package a3;
+
+public class Datum {
+    public void main(String[] args) {
+        
+    }
+}
